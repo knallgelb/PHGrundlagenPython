b616fe2da27d4b2992588b5dcf056cf8ebed795b
Rainer Amler <rainer@amler.at>
2020-10-01 17:07:40 +0200

    typo

--- hangman.py
+++ hangman.py
@@ -57,3 +57,28 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
+
+iterator = iter(HANGMANPICS)
+
+from getpass import getpass
+
+geheimnis = getpass("dein Geheimnis\n")
+charaters_found = []
+
+def print_guess(geheimnis, values):
+    return "_".join(x for x in geheimnis if x in values)
+
+while True:
+    guess_char = input("Dein Vorschlag?\n")
+    if guess_char not in geheimnis:
+        try:
+            print(next(iterator))
+        except StopIteration:
+            print("Game Over")
+            break
+    else:
+        charaters_found.append(guess_char)
+        if len(set(charaters_found)) == len(set(geheimnis)):
+            print("Gewonnen :)")
+            break
+    print(print_guess(geheimnis, charaters_found))
